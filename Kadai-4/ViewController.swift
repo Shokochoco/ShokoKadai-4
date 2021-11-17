bf40551147ecd4587839877c7c721d3813777779
Shokochoco <shsuzu1992@gmail.com>
2021-11-17 11:16:50 +0900

    Initial Commit

---
+++ Kadai-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai-4
+//
+//  Created by 鈴木淳子 on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
